dd0efc032d7e98ad4376cbd2f57fd56278d41bb5
kliment2000 <klimmitrofanov.2000@gmail.com>
2025-08-19 21:26:23 +0800

    done l1_15

---
+++ l1_15/l1_15.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+var justString string
+
+func createHugeString(size int) string {
+	return string(make([]byte, size))
+}
+
+func someFuncLeak() {
+	v := createHugeString(100 << 20)
+	justString = v[:100]
+	fmt.Println("Вызывается функция")
+}
+
+func someFuncFixed() {
+	v := createHugeString(100 << 20)
+	justString = string([]byte(v[:100]))
+	fmt.Println("Вызывается функция")
+}
+
+func printMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	fmt.Printf("Занято %v MB\n", m.Alloc/1024/1024)
+}
+
+func main() {
+	fmt.Println("В изначальном варианте происходит утечка")
+	printMemUsage()
+	someFuncLeak()
+	runtime.GC()
+	printMemUsage()
+
+	justString = ""
+	runtime.GC()
+
+	fmt.Println("\nКорректный пример реализации")
+	printMemUsage()
+	someFuncFixed()
+	runtime.GC()
+	printMemUsage()
+
+	_ = justString // чтоб GoLand не ругался
+}
