4370fe36c8bfbd6837b717d74b7be49e84ee393f
kliment2000 <klimmitrofanov.2000@gmail.com>
2025-08-24 21:37:28 +0800

    done l1_18

---
+++ l1_18/l1_18.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+type Counter struct {
+	value int64
+}
+
+func (c *Counter) Inc() {
+	atomic.AddInt64(&c.value, 1)
+}
+
+func (c *Counter) Value() int64 {
+	return atomic.LoadInt64(&c.value)
+}
+
+func main() {
+	var wg sync.WaitGroup
+	counter := &Counter{}
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			counter.Inc()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("Итоговое значение счетчика:", counter.Value())
+}
