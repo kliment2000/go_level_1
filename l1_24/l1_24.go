01cbc7673b6ef299bd7eb7212b911510e5657bba
kliment2000 <klimmitrofanov.2000@gmail.com>
2025-08-30 02:08:36 +0800

    done l1_24

---
+++ l1_24/l1_24.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	x float64
+	y float64
+}
+
+func NewPoint(x, y float64) Point {
+	return Point{x: x, y: y}
+}
+
+func (p Point) Distance(other Point) float64 {
+	dx := p.x - other.x
+	dy := p.y - other.y
+	return math.Sqrt(dx*dx + dy*dy)
+}
+
+func main() {
+	a := NewPoint(1, 0)
+	b := NewPoint(4, 4)
+
+	dist := a.Distance(b)
+	fmt.Println(dist)
+}
