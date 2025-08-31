a4c391f123bea0326149fc75cc155b366e269e4f
kliment2000 <klimmitrofanov.2000@gmail.com>
2025-09-01 01:10:17 +0800

    done l1_23

--- l1_23/l1_23.go
+++ l1_23/l1_23.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"runtime"
-	"strings"
 )
 
 func printMemUsage() {
@@ -13,22 +12,21 @@ func printMemUsage() {
 	fmt.Printf("Занято %v MB\n", m.Alloc/1024/1024)
 }
 
+type Big struct {
+	data [1024 * 1024]int
+}
+
 func main() {
-	huge := strings.Repeat("A", 10*1024*1024)
-
-	s := []string{"Go", "Rust", "Python", huge}
+	s := make([]*Big, 3)
+	for i := range s {
+		s[i] = &Big{}
+	}
 	printMemUsage()
 
-	// Вариант 1: с утечкой
-	s1 := s
-	copy(s1[2:], s1[3:])
-	s1 = s1[:len(s1)-1]
-	printMemUsage()
+	i := 1
 
-	// Вариант 2: без утечки
-	s2 := s
-	copy(s2[2:], s2[3:])
-	s2[len(s2)-1] = ""
-	s2 = s2[:len(s2)-1]
+	copy(s[i:], s[i+1:])
+	s = s[:len(s)-1]
 	printMemUsage()
+	fmt.Println(s)
 }
